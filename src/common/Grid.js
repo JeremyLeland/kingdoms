dbb7f339a2778894e69c8b506763af439bb7a84f
Jeremy Leland <jeremy.leland@gmail.com>
2025-01-25 14:27:14 -0800

    Seem to have fixed performance issues for now by going back to how I used to do things (save/restore instead of undoing translate, make a Path object instead of doing the moves each time)

--- src/common/Grid.js
+++ src/common/Grid.js
@@ -1,42 +1,58 @@
-export function draw( ctx, minX, minY, maxX, maxY ) {
-  ctx.save(); {
+export class Grid {
+  #path;
 
-    // Make it look like:
-    // + - - +
-    // |     |
-    // |     |
-    // + - - +
-    // ctx.setLineDash( [ 0.1, 0.2, 0.1, 0.2, 0.1, 0.2, 0.1, 0 ] );
-    ctx.fillStyle = ctx.strokeStyle = '#ccca';
-
-    for ( let col = minX; col <= maxX + 1; col ++ ) {
-      ctx.moveTo( col - 0.5, minY - 0.5 );
-      ctx.lineTo( col - 0.5, maxY + 0.5 );
-    }
-
-    for ( let row = minY; row <= maxY + 1; row ++ ) {
-      ctx.moveTo( minX - 0.5, row - 0.5 );
-      ctx.lineTo( maxX + 0.5, row - 0.5 );
-    }
-
-    ctx.stroke();
-
-    ctx.font = '0.2px Arial';
-    ctx.textAlign = 'center';
-    // ctx.textBaseline = 'middle';
-
-    // ctx.translate( minX, minY );
-    for ( let row = minY; row <= maxY; row ++ ) {
-      // ctx.save(); {
-        for ( let col = minX; col <= maxX; col ++ ) {
-          ctx.fillText( `(${ col },${ row })`, col, row );
-          // ctx.translate( 1, 0 );
-        }
-      // }
-      // ctx.restore();
-
-      // ctx.translate( 0, 1 );
-    }
+  constructor( minX, minY, maxX, maxY ) {
+    this.#path = getGrid( minX, minY, maxX, maxY );
   }
-  ctx.restore();
+
+  draw( ctx ) {
+  // ctx.save(); {
+
+      // Make it look like:
+      // + - - +
+      // |     |
+      // |     |
+      // + - - +
+      // ctx.setLineDash( [ 0.1, 0.2, 0.1, 0.2, 0.1, 0.2, 0.1, 0 ] );
+      ctx.fillStyle = ctx.strokeStyle = '#ccca';
+
+      ctx.stroke( this.#path );
+
+      if ( false ) {
+        ctx.font = '0.2px Arial';
+        ctx.textAlign = 'center';
+        // ctx.textBaseline = 'middle';
+
+        // ctx.translate( minX, minY );
+        for ( let row = minY; row <= maxY; row ++ ) {
+          // ctx.save(); {
+            for ( let col = minX; col <= maxX; col ++ ) {
+              ctx.fillText( `(${ col },${ row })`, col, row );
+              // ctx.translate( 1, 0 );
+            }
+          // }
+          // ctx.restore();
+
+          // ctx.translate( 0, 1 );
+        }
+      }
+    // }
+    // ctx.restore();
+  }
+}
+
+function getGrid( minX, minY, maxX, maxY ) {
+  const grid = new Path2D();
+
+  for ( let col = minX; col <= maxX + 1; col ++ ) {
+    grid.moveTo( col - 0.5, minY - 0.5 );
+    grid.lineTo( col - 0.5, maxY + 0.5 );
+  }
+
+  for ( let row = minY; row <= maxY + 1; row ++ ) {
+    grid.moveTo( minX - 0.5, row - 0.5 );
+    grid.lineTo( maxX + 0.5, row - 0.5 );
+  }
+
+  return grid;
 }
