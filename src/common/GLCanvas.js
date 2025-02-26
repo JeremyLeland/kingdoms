4c2c59c85ad27f454fd10b899a1e73c177a42656
Jeremy Leland <jeremy.leland@gmail.com>
2025-02-26 14:12:54 -0800

    Use resizeObserver for canvas

--- src/common/GLCanvas.js
+++ src/common/GLCanvas.js
@@ -1,13 +1,17 @@
 export class GLCanvas {
   #reqId;
 
-  constructor( width, height, canvas ) {
-    this.canvas = canvas ?? document.createElement( 'canvas' );
-    this.canvas.oncontextmenu = () => { return false };
-
-    if ( !canvas ) {
+  constructor( canvas ) {
+    this.canvas = canvas;
+    
+    if ( !this.canvas ) {
+      this.canvas = document.createElement( 'canvas' );
       document.body.appendChild( this.canvas );
     }
+    this.canvas.style.width = '100%';
+    this.canvas.style.height = '100%';
+    
+    this.canvas.oncontextmenu = () => { return false };
     
     this.gl = this.canvas.getContext( 'webgl2' );
 
@@ -15,27 +19,34 @@ export class GLCanvas {
       alert( "Unable to initialize WebGL. Your browser or machine may not support it." );
     }
 
-    this.gl.clearColor( 0.0, 0.0, 0.5, 1.0 );
+    this.gl.clearColor( 0.0, 0.0, 0.2, 1.0 );
     this.gl.clearDepth( 1.0 );
     this.gl.enable( this.gl.DEPTH_TEST );
     this.gl.depthFunc( this.gl.LEQUAL );
 
-    if ( width && height ) {
-      this.setSize( width, height );
-    }
-    else {
-      window.onresize = () => this.setSize( window.innerWidth, window.innerHeight );
-      window.onresize();
-    }
-  }
+    //
+    // Resize with parent element
+    //
 
-  setSize( width, height ) {
-    this.canvas.width = width * devicePixelRatio;
-    this.canvas.height = height * devicePixelRatio;
-    this.canvas.style.width = width + 'px';
-    this.canvas.style.height = height + 'px';
+    const resizeObserver = new ResizeObserver( entries => {
+      entries.forEach( entry => {
+        // safari does not support devicePixelContentBoxSize, attempting to work around
+        const width = entry.devicePixelContentBoxSize?.[ 0 ].inlineSize ?? ( entry.contentBoxSize[ 0 ].inlineSize * devicePixelRatio );
+        const height = entry.devicePixelContentBoxSize?.[ 0 ].blockSize ?? ( entry.contentBoxSize[ 0 ].blockSize * devicePixelRatio );
+        this.canvas.width = width;
+        this.canvas.height = height;
 
-    this.gl.viewport( 0, 0, this.canvas.width, this.canvas.height );
+        // this still needs to be based on content box
+        const inlineSize = entry.contentBoxSize[ 0 ].inlineSize;
+        const blockSize = entry.contentBoxSize[ 0 ].blockSize;
+
+        this.gl.viewport( 0, 0, width, height );
+      } );
+      
+      this.redraw();
+    } );
+
+    resizeObserver.observe( this.canvas );
   }
 
   redraw() {
@@ -47,22 +58,27 @@ export class GLCanvas {
   //       check if reqId not null? (set null in stop)
 
   start() {
-    let lastTime;
-    const animate = ( now ) => {
-      lastTime ??= now;  // for first call only
-      this.update( now - lastTime );
-      lastTime = now;
-  
-      this.redraw();
-  
-      this.#reqId = requestAnimationFrame( animate );
-    };
+    if ( !this.#reqId ) {     // don't try to start again if already started
+      let lastTime;
+      const animate = ( now ) => {
+        lastTime ??= now;  // for first call only
+        this.update( now - lastTime );
+        lastTime = now;
+    
+        this.redraw();
+    
+        if ( this.#reqId ) {    // make sure we didn't stop it
+          this.#reqId = requestAnimationFrame( animate );
+        }
+      };
 
-    this.#reqId = requestAnimationFrame( animate );
+      this.#reqId = requestAnimationFrame( animate );
+    }
   }
 
   stop() {
     cancelAnimationFrame( this.#reqId );
+    this.#reqId = null;   // so we can check if stopped
   }
 
   update( dt ) {}
