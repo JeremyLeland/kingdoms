240870f948bee8f3df8e65e86e8633f6ac15c5f6
Jeremy Leland <jeremy.leland@gmail.com>
2025-04-07 13:43:54 -0700

    Move drawing code into separate file

---
+++ src/Entity.js
@@ -0,0 +1,159 @@
+import { mat4 } from '../lib/gl-matrix.js';
+
+
+import * as MeshCommon from '../src/common/MeshCommon.js';
+import * as ShaderCommon from '../src/common/ShaderCommon.js';
+
+import * as Buildings from '../src/entities/Buildings.js';
+import * as Resources from '../src/entities/Resources.js';
+import * as Worker from '../src/entities/Worker.js';
+
+const ModelInfo = {
+  'Wood': Resources.WoodModel,
+  'Stone': Resources.StoneModel,
+  'Stockpile': Buildings.StockpileModel,
+  'Worker': Worker.Model,
+};
+
+const Meshes = new Map();
+
+let shader;
+
+function applyTransforms( matrix, transform ) {
+  if ( transform.pos ) {
+    mat4.translate( matrix, matrix, transform.pos );
+  }
+
+  if ( transform.rot ) {
+    mat4.rotate( matrix, matrix, transform.rot[ 2 ], [ 0, 0, 1 ] );
+    mat4.rotate( matrix, matrix, transform.rot[ 1 ], [ 0, 1, 0 ] );
+    mat4.rotate( matrix, matrix, transform.rot[ 0 ], [ 1, 0, 0 ] );
+  }
+
+  if ( transform.scale ) {
+    mat4.scale( matrix, matrix, transform.scale );
+  }
+}
+
+const mvp = mat4.create();
+const normalMatrix = mat4.create();
+
+const blend = {
+  pos: [ 0, 0, 0 ],
+  scale: [ 1, 1, 1 ],
+};
+
+export function draw( entity, gl, modelMatrixStack, projViewMatrix ) {
+
+  modelMatrixStack.save();
+
+  applyTransforms( modelMatrixStack.current, entity );
+
+  const modelInfo = ModelInfo[ entity.type ];
+
+  for ( const part in modelInfo ) {
+    const partInfo = modelInfo[ part ];
+
+    modelMatrixStack.save();
+
+    if ( entity.animation ) {
+
+      // TODO: Where should animation specific transforms go? Here?
+      // Using fill to avoid garbage collection
+      blend.pos.fill( 0 );
+      blend.scale.fill( 1 );
+      
+      const keyframes = partInfo.keyframes?.[ entity.animation.name ];
+
+      if ( keyframes ) {
+        const totalTime = keyframes[ keyframes.length - 1 ].time;
+        const loopTime = entity.animation.time % totalTime;
+        
+        let nextIndex = 0;
+        for ( let i = 0; i < keyframes.length; i ++ ) {
+          if ( keyframes[ i ].time > loopTime ) {
+            nextIndex = i;
+            break;
+          }
+        }
+
+        // NOTE: This assumes a keyframe at zero. Should we create one automatically?
+        const prevFrame = keyframes[ nextIndex - 1 ];
+        const nextFrame = keyframes[ nextIndex ];
+
+        const partialTime = ( entity.animation.time - prevFrame.time ) / ( nextFrame.time - prevFrame.time );
+
+        for ( let i = 0; i < 3; i ++ ) {
+          if ( prevFrame.pos ) {
+            blend.pos[ i ] = prevFrame.pos[ i ] + partialTime * ( nextFrame.pos[ i ] - prevFrame.pos[ i ] );
+          }
+
+          if ( prevFrame.scale ) {
+            blend.scale[ i ] = prevFrame.scale[ i ] + partialTime * ( nextFrame.scale[ i ] - prevFrame.scale[ i ] );
+          }
+        }
+
+        applyTransforms( modelMatrixStack.current, blend );
+      }
+    }
+
+    applyTransforms( modelMatrixStack.current, partInfo );
+
+    mat4.multiply( mvp, projViewMatrix, modelMatrixStack.current );
+
+    mat4.invert( normalMatrix, modelMatrixStack.current );
+    mat4.transpose( normalMatrix, normalMatrix );
+
+    // TODO: Map of shaders, like Map of meshes below
+    if ( shader == null ) {
+      shader = ShaderCommon.getShader( gl, ShaderCommon.BasicLighting );
+    }
+
+    gl.useProgram( shader.program );
+    gl.uniformMatrix4fv( shader.uniformLocations.mvp, false, mvp );
+    gl.uniformMatrix4fv( shader.uniformLocations.normalMatrix, false, normalMatrix );
+
+    if ( partInfo.color ) {
+      gl.uniform3fv( shader.uniformLocations.color, partInfo.color );
+    }
+
+    if ( partInfo.mesh ) {
+      if ( !Meshes.has( part ) ) {
+        Meshes.set( part, MeshCommon.getMesh( gl, partInfo.mesh ) );
+      }
+      
+      const mesh = Meshes.get( part );
+      
+      gl.bindBuffer( gl.ARRAY_BUFFER, mesh.positionBuffer );
+      gl.vertexAttribPointer( shader.attribLocations.position, 3, gl.FLOAT, false, 0, 0 );
+      gl.enableVertexAttribArray( shader.attribLocations.position );
+      
+      gl.bindBuffer( gl.ARRAY_BUFFER, mesh.normalBuffer );
+      gl.vertexAttribPointer( shader.attribLocations.normal, 3, gl.FLOAT, false, 0, 0 );
+      gl.enableVertexAttribArray( shader.attribLocations.normal );
+      
+      gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, mesh.indexBuffer );
+      gl.drawElements( gl.TRIANGLES, mesh.length, gl.UNSIGNED_SHORT, 0 );
+    }
+
+    if ( partInfo.attach ) {
+      const attachList = entity[ partInfo.attach ];
+
+      // TODO: attachFunc to iterate through list and draw? (so carry stack is done differently than pile stack, e.g.)
+      attachList.forEach( attached => {
+        draw( attached, gl, modelMatrixStack, projViewMatrix );
+
+        // TODO: Get size from ModelInfo? Make it top level somehow? (may need an overall bounding box for selection anyway)
+        mat4.translate( modelMatrixStack.current, modelMatrixStack.current, [ 0, 0.15, 0 ] );
+
+        // How to make wood lay flat on top of ground (and not partway in it )?
+        // Should all wood be offset by half its height so that 0 is the bottom instead of center?
+
+      } );
+    }
+
+    modelMatrixStack.restore();
+  }
+
+  modelMatrixStack.restore();
+}
